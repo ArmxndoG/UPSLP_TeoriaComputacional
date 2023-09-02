a5eb1e7f03616a42092fb4e320236ef5d83de0f4
ArmxndoG <143360795+ArmxndoG@users.noreply.github.com>
2023-09-01 20:59:24 -0600

    codificación

--- ConstanteKaprekar.py
+++ ConstanteKaprekar.py
@@ -1,28 +1,5 @@
-'''
-Consiste simplemente en reordenar los dígitos de un número de modo que se obtenga el mayor y el menor número posible, restando entonces el menor del mayor.
 
-Si cogemos los cuatro dígitos de 6174 y formamos dos nuevos números ordenándolos en orden descendente y ascendente, obtenemos 7641 y 1467. Al restar, 
-obtenemos 7641 – 1467 = 6174, el número desde el cual comenzamos.
-
-Esto no es una gran sorpresa. Lo que sorprende es que, comenzando desde cualquier otro número de cuatro dígitos (que no sea «repdigits«, como 3333, 
-debido a que su sustracción resulta en cero) y repitiendo el mismo proceso, siempre llegamos al 6174. Por ejemplo:
-
-Cogemos el número 1234 y aplicamos el mismo proceso:
-4321 – 1234 = 3087
-
-8730 – 0378 = 8352
-
-8532 – 2358 = 6174
-
-
-Nuevamente, si elegimos el año actual, 2018, obtenemos la secuencia:
-8210 – 0128 = 8082
-
-8820 – 0288 = 8532
-
-8532 – 2358 = 6174
-'''
-
+#coding=utf-8
 def constanteKaprekar(numero):
     
     digitos = [int(a) for a in str(numero)]#guardando los digitos en una lista ej: [1,2,3,4]
