24a798454ee3afe543ceade6aec06ebaabe2a5b2
ArmxndoG <143360795+ArmxndoG@users.noreply.github.com>
2023-09-01 20:51:29 -0600

    Update ConjeturaCollatz.py

--- ConjeturaCollatz.py
+++ ConjeturaCollatz.py
@@ -10,12 +10,12 @@ def conjeturaCollatz(numero):
         numero = (3*numero) + 1
         
     print(numero)
-    if numero != 1: #Volver a llamar la función hasta que el numero sea 1
+    if numero != 1: #Volver a llamar la funcion hasta que el numero sea 1
         conjeturaCollatz(numero)
     else:
         return numero
         
-numero_entero = int(input("Digita un número entero: "))
+numero_entero = int(input("Digita un numero entero: "))
 serie = conjeturaCollatz(numero_entero)
 
 
