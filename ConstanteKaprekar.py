11e0c3aaac4fa4f725a9fc65fde5cdb27d62fbff
ArmxndoG <143360795+ArmxndoG@users.noreply.github.com>
2023-09-01 21:16:56 -0600

    Update ConstanteKaprekar.py

--- ConstanteKaprekar.py
+++ ConstanteKaprekar.py
@@ -7,18 +7,17 @@ def constanteKaprekar(numero):
     menor = int(''.join(map(str,sorted(digitos)))) #concatenar los digitos de la lista como cadenas para luego convertir esa cadena a 'enter ej: [1,2,3,4] -> '1234' -> 1234
     mayor = int(''.join(map(str,sorted(digitos,reverse=True)))) #[4,3,2,1] -> '4321' -> 4321
     resta = mayor - menor #Restar el menor al mayor
-    print(f"{mayor} - {menor} = {resta}")
+    #print(f"{mayor} - {menor} = {resta}")
+    print('{} - {} = {}'.format(mayor,menor,resta))
     #Volver a llamar a la función hasta que el resultado sea 6174
     if resta == 6174: 
-        print(f"Se alcanzó la constante de kaprekar: {resta}")
+        #print(f"Se alcanzó la constante de kaprekar: {resta}")
+        print("Se alcanzó la constante de kaprekar: {}".format(resta))
         return resta
     else:
         constanteKaprekar(resta)
-# Verificar que el número sea de 4 digitos   
-while(True):
-    pedir_numero = input("Digita un número de 4 digitos: ")
-    if(len(pedir_numero)<=4):break
 
+pedir_numero = input("Digita un número de 4 digitos: ")
 numero = constanteKaprekar(int(pedir_numero))
 
 
